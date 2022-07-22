64753acad854167e74b5255325ee37ff8063ec03
Joao paulo Gomes da Silva <joaopaulopai99@gmail.com>
2022-07-21 21:28:44 -0400

    commit 21/07/22 iniciando curso com Luiz Otavio

---
+++ LuizOtavio/modulo1/aula1/main.py
@@ -0,0 +1,14 @@
+# Linha de comentários
+
+print('ola mundo')
+
+print('linha 1')  # comentário depois do código
+
+print('linha 2')  # outro comentario
+
+print('linha 3')
+
+print('linha 4')
+
+print('linha 5')
+print('linha 6')
